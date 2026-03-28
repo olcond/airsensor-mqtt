1278f381e79fdaf24df402a0103eb7c93996671c
volschin <volschin@googlemail.com>
2026-03-28 05:59:42 +0000

    chore: switch Docker CI to ghcr.io and bump APP_VERSION to 1.0.2
    Migrate container registry from Docker Hub to GitHub Container Registry
    using GITHUB_TOKEN auth and docker/metadata-action for automated tagging.

--- airsensor.h
+++ airsensor.h
@@ -16,7 +16,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define APP_VERSION "1.0.1"
+#define APP_VERSION "1.0.2"
 
 /* --------------------------------------------------------------------------
  * Device data types
